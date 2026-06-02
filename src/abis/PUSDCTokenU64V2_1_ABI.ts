889ba58cc9589b6b14ae5fc24feac050ad24b419
cyf <yuanfeng@padolabs.org>
2026-06-02 11:59:43 +0800

    chroe: update PUSDCTokenU64V2_1_ABI.ts

--- src/abis/PUSDCTokenU64V2_1_ABI.ts
+++ src/abis/PUSDCTokenU64V2_1_ABI.ts
@@ -1,24 +1,35 @@
 export const PUSDCTokenU64V2_1_ABI = [
     {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
       "inputs": [
         {
-          "internalType": "string",
-          "name": "name_",
-          "type": "string"
-        },
-        {
-          "internalType": "string",
-          "name": "symbol_",
-          "type": "string"
-        },
-        {
           "internalType": "address",
-          "name": "_erc20_address",
+          "name": "target",
           "type": "address"
         }
       ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
+      "name": "AddressEmptyCode",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "implementation",
+          "type": "address"
+        }
+      ],
+      "name": "ERC1967InvalidImplementation",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "ERC1967NonPayable",
+      "type": "error"
     },
     {
       "inputs": [
@@ -108,7 +119,17 @@ export const PUSDCTokenU64V2_1_ABI = [
     },
     {
       "inputs": [],
+      "name": "FailedCall",
+      "type": "error"
+    },
+    {
+      "inputs": [],
       "name": "HandlesAlreadySavedForRequestID",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "InvalidInitialization",
       "type": "error"
     },
     {
@@ -122,17 +143,47 @@ export const PUSDCTokenU64V2_1_ABI = [
       "type": "error"
     },
     {
-      "anonymous": false,
+      "inputs": [],
+      "name": "NotInitializing",
+      "type": "error"
+    },
+    {
       "inputs": [
         {
-          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnableInvalidOwner",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
           "internalType": "address",
           "name": "account",
           "type": "address"
         }
       ],
-      "name": "AddedToWhitelist",
-      "type": "event"
+      "name": "OwnableUnauthorizedAccount",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "UUPSUnauthorizedCallContext",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "slot",
+          "type": "bytes32"
+        }
+      ],
+      "name": "UUPSUnsupportedProxiableUUID",
+      "type": "error"
     },
     {
       "anonymous": false,
@@ -176,13 +227,13 @@ export const PUSDCTokenU64V2_1_ABI = [
       "anonymous": false,
       "inputs": [
         {
-          "indexed": true,
-          "internalType": "address",
-          "name": "account",
-          "type": "address"
+          "indexed": false,
+          "internalType": "uint64",
+          "name": "version",
+          "type": "uint64"
         }
       ],
-      "name": "OwnershipTransfer",
+      "name": "Initialized",
       "type": "event"
     },
     {
@@ -191,11 +242,17 @@ export const PUSDCTokenU64V2_1_ABI = [
         {
           "indexed": true,
           "internalType": "address",
-          "name": "account",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
           "type": "address"
         }
       ],
-      "name": "RemovedFromWhitelist",
+      "name": "OwnershipTransferred",
       "type": "event"
     },
     {
@@ -224,6 +281,32 @@ export const PUSDCTokenU64V2_1_ABI = [
       "type": "event"
     },
     {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "implementation",
+          "type": "address"
+        }
+      ],
+      "name": "Upgraded",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "UPGRADE_INTERFACE_VERSION",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
       "inputs": [
         {
           "internalType": "address",
@@ -232,19 +315,6 @@ export const PUSDCTokenU64V2_1_ABI = [
         }
       ],
       "name": "addOracle",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_account",
-          "type": "address"
-        }
-      ],
-      "name": "addToWhitelist",
       "outputs": [],
       "stateMutability": "nonpayable",
       "type": "function"
@@ -432,48 +502,26 @@ export const PUSDCTokenU64V2_1_ABI = [
       "type": "function"
     },
     {
-      "inputs": [],
-      "name": "getFullWhitelist",
-      "outputs": [
-        {
-          "internalType": "address[]",
-          "name": "",
-          "type": "address[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "getTotalHandles",
-      "outputs": [
-        {
-          "internalType": "ve_uint64[]",
-          "name": "",
-          "type": "bytes32[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
       "inputs": [
         {
+          "internalType": "string",
+          "name": "name_",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "symbol_",
+          "type": "string"
+        },
+        {
           "internalType": "address",
-          "name": "_account",
+          "name": "_erc20_address",
           "type": "address"
         }
       ],
-      "name": "isWhitelisted",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
+      "name": "initialize",
+      "outputs": [],
+      "stateMutability": "nonpayable",
       "type": "function"
     },
     {
@@ -503,16 +551,16 @@ export const PUSDCTokenU64V2_1_ABI = [
       "type": "function"
     },
     {
-      "inputs": [
+      "inputs": [],
+      "name": "proxiableUUID",
+      "outputs": [
         {
-          "internalType": "address",
-          "name": "_account",
-          "type": "address"
+          "internalType": "bytes32",
+          "name": "",
+          "type": "bytes32"
         }
       ],
-      "name": "removeFromWhitelist",
-      "outputs": [],
-      "stateMutability": "nonpayable",
+      "stateMutability": "view",
       "type": "function"
     },
     {
@@ -524,6 +572,13 @@ export const PUSDCTokenU64V2_1_ABI = [
         }
       ],
       "name": "removeOracle",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
       "outputs": [],
       "stateMutability": "nonpayable",
       "type": "function"
@@ -652,6 +707,24 @@ export const PUSDCTokenU64V2_1_ABI = [
       "name": "transferOwnership",
       "outputs": [],
       "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newImplementation",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes",
+          "name": "data",
+          "type": "bytes"
+        }
+      ],
+      "name": "upgradeToAndCall",
+      "outputs": [],
+      "stateMutability": "payable",
       "type": "function"
     }
   ];
